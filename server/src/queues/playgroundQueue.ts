8b6408bc831051c66e54456add384e8d29023f27
Vallabh Mahajan <vallabhmahajan570@gmail.com>
2026-07-29 19:03:14 +0530

    refactor: initialize playground queue

---
+++ server/src/queues/playgroundQueue.ts
@@ -0,0 +1,35 @@
+import { Queue, QueueEvents } from 'bullmq';
+import { Server } from 'socket.io';
+import { ENV } from '../config/env';
+import { PlaygroundJobData } from '../types/index';
+// import { handlePlaygroundCompletion, handlePlaygroundFailure } from '../handlers/PlaygroundResultHandler';
+import { redisConnection } from '../config/redis';
+
+const QUEUE_NAME = ENV.PLAYGROUND_QUEUE_NAME;
+
+// Queue Producer
+export const playgroundQueue = new Queue<PlaygroundJobData>(QUEUE_NAME, {
+    connection : redisConnection as any
+});
+
+// inject IO instance to broadcast results
+export const setupPlaygroundQueueListeners = (io: Server) => {
+    // Queue listener
+    const queueEvents = new QueueEvents(QUEUE_NAME, {
+        connection: redisConnection as any
+    });
+
+    queueEvents.waitUntilReady().then(() => {
+        console.log(`[QueueEvents] Ready and listening to queue: ${QUEUE_NAME}`);
+    });
+
+    queueEvents.on('completed', async ({ jobId, returnvalue }: any) => {
+        // await handlePlaygroundCompletion(io, playgroundQueue, jobId, returnvalue);        
+    });
+
+
+    queueEvents.on('failed', async ({ jobId, failedReason }: any) => {
+        // await handlePlaygroundFailure(io, playgroundQueue, jobId, failedReason);    
+        });
+};
+
